0ebfefed0636e74b85d9a3c6035fb340eb745bee
udairhythm <undoshola@gmail.com>
2023-05-28 19:32:08 +0900

    2023/05/28

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,64 +1,47 @@
-
-//金額のコンマ
-/**
- * フォーマット済みテキストを返す関数
- * @param {string} rawValue 元のテキスト
- * @param {number} fractionDigits 小数点以下固定桁数
- * @return {string} フォーマット済みテキストor空白文字（数値以外の場合）
- */
-
-
-function get_fotmat_input_digit(rawValue, fractionDigits) {
-    if (rawValue === '') {
-        //ユーザーが空白にしたなら空白にして終了
-        return '';
-    }
-    //入力値を数値だけ半角変換
-    let halfWidthVal = rawValue.replace(/[０-９]/g, function(x) {
-        return String.fromCharCode(x.charCodeAt(0) - 0xFEE0);
-    });
-    //カンマを除去（入力値が数値か否かを確認のため）
-    let commaRemovedVal = halfWidthVal.replaceAll(',', '').replaceAll('、', '');
-    if (isNaN(commaRemovedVal)) {
-        //数値でなければ空白にする
-        return '';
-    } else {
-        //入力値をカンマ入り数字テキストに再変換
-        return new Intl.NumberFormat('ja-JP', {maximumFractionDigits: fractionDigits, minimumFractionDigits: fractionDigits}).format(commaRemovedVal);
-    }
-}
-
-
-$(function() {
-    //フォーカスが外れた時に入力値をフォーマット済み文字列に変換するイベントを定義
-    //整数用クラス
-    $(document).on('blur', '.sakai-digit-0', function() {
-        $(this).val(get_fotmat_input_digit($(this).val(), 0));
-    });
-});
-
-
-//フォーカス時にカンマを除去
-$(document).on('focus', '.sakai-digit', function() {
-    //入力値取得
-    let val = $(this).val();
-    //カンマ除去
-    let comma_removed_val = val.replaceAll(',', '');
-    if (isNaN(comma_removed_val) || val.indexOf(',,') != -1) {
-        //数値でなければ処理をしない
-        return;
-    }
-    $(this).val(comma_removed_val);
-});
-
-
-//Saveボタン
+//Saveボタンを押して入力値をローカルストレージに保存
 $("#save").on("click", function() {
     //入力された文字を取得したいと思います
-    const key = $("#key").val();
-    //コンソール
-    console.log(key, 'keyの中身')
+    const date = $("#date-input").val();
+    const category = $("#category").val();
+    const details = $("#details").val();
+    const money = $("#money").val();
 
+    const entry = {
+        date: date,
+        category: category,
+        details: details,
+        money: money,
+    };
+
+    localStorage.setItem(date, JSON.stringify(entry));
+});
+
+
+
+//日付がデフォルトで今日
+var date = new Date();
+
+var yyyy = date.getFullYear();
+var mm = ("0"+(date.getMonth()+1)).slice(-2);
+var dd = ("0"+date.getDate()).slice(-2);
+
+document.getElementById("date-input").value=yyyy+'-'+mm+'-'+dd;
+
+
+//金額のコンマ
+
+function kanmaChange(inputAns){
+    console.log(inputAns);
+    let inputAnsValue = inputAns.value;
+    console.log(inputAnsValue);
+    let numberAns = inputAnsValue.replace(/[^0-9]/g, "");
+    kanmaAns = numberAns.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
+    console.log(kanmaAns);
+    if(kanmaAns.match(/[^0-9]/g)){
+     inputAns.value= kanmaAns;
+     return true;
+    }
+   };
 
 
 // $("#save").on("click", function()) {
